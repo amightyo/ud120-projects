febe5cf5ba6640ce3ef146e1f39e2bbb6d7a1357
Mighty Itauma <amightyo@gmail.com>
2015-01-28 18:20:46 -0500

    training on only 1% of the full training set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -35,6 +35,8 @@ clf = SVC(kernel="linear")
 #### now your job is to fit the classifier
 #### using the training features/labels, and to
 #### make a set of predictions on the test data
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100] 
 clf.fit(features_train, labels_train)
 
 
